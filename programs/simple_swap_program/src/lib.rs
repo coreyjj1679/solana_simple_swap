21e937b69944019303770e41a8403d9f926d7fad
coreyjj1679 <ruggedev@gmail.com>
2024-11-11 14:53:37 +0800

    add simple depositor, simple test

--- programs/simple_swap_program/src/lib.rs
+++ programs/simple_swap_program/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use solana_program::system_instruction;
 
 declare_id!("13avuvj2qnHq6CwsuYFR7jLrKbbzgGXxscfZCBQR7kJW");
 
@@ -6,11 +7,62 @@ declare_id!("13avuvj2qnHq6CwsuYFR7jLrKbbzgGXxscfZCBQR7kJW");
 pub mod simple_swap_program {
     use super::*;
 
+    // init valut to store SOL
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+        let vault = &mut ctx.accounts.vault;
+
+        // will be the deployer in this sceranio
+        vault.authority = ctx.accounts.admin.key();
+        vault.balance = 0; // init bal
+
+        Ok(())
+    }
+
+    pub fn deposit_sol(ctx: Context<Deposit>, amount: u64) -> Result<()> {
+        let admin = &ctx.accounts.admin;
+
+        let transfer_instruction = system_instruction::transfer(
+            admin.key,
+            ctx.accounts.vault.to_account_info().key,
+            amount,
+        );
+
+        anchor_lang::solana_program::program::invoke_signed(
+            &transfer_instruction,
+            &[
+                admin.to_account_info(),
+                ctx.accounts.vault.to_account_info(),
+                ctx.accounts.system_program.to_account_info(),
+            ],
+            &[],
+        )?;
+
+        let vault = &mut ctx.accounts.vault;
+        vault.balance += amount;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = admin, space = 8 + 32 + 8)]
+    pub vault: Account<'info, Vault>, // Vault account
+    #[account(mut)]
+    pub admin: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Deposit<'info> {
+    pub admin: Signer<'info>,
+    #[account(mut)]
+    pub vault: Account<'info, Vault>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Vault {
+    pub authority: Pubkey, // admin
+    pub balance: u64,
+}
--- tests/simple_swap_program.ts
+++ tests/simple_swap_program.ts
@@ -3,14 +3,38 @@ import { Program } from "@coral-xyz/anchor";
 import { SimpleSwapProgram } from "../target/types/simple_swap_program";
 
 describe("simple_swap_program", () => {
-  // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.SimpleSwapProgram as Program<SimpleSwapProgram>;
+  const program = anchor.workspace
+    .SimpleSwapProgram as Program<SimpleSwapProgram>;
 
+  let admin: anchor.web3.Keypair;
+  let vault: anchor.web3.Keypair;
+
+  before(async () => {
+    admin = anchor.web3.Keypair.generate();
+    vault = anchor.web3.Keypair.generate();
+
+    // get SOL for the gas
+    const provider = anchor.getProvider();
+    await provider.connection.confirmTransaction(
+      await provider.connection.requestAirdrop(
+        admin.publicKey,
+        anchor.web3.LAMPORTS_PER_SOL
+      ),
+      "confirmed"
+    );
+  });
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const tx = await program.methods
+      .initialize()
+      .accounts({
+        vault: vault.publicKey,
+        admin: admin.publicKey,
+      })
+      .signers([admin, vault])
+      .rpc();
     console.log("Your transaction signature", tx);
   });
 });
